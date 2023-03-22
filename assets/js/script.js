f9a432ad874b3f2f06b5bb6f4a8dc4ac1844f9ec
michael-megan <sba21652@student.cct.ie>
2023-03-22 23:47:49 +0000

    change divide so remainder does not occur in game.

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,7 +142,6 @@ function displayDivisionQuestion(operand1, operand2) {
 	document.getElementById('operand1').textContent = operand1;
 	document.getElementById('operand2').textContent = operand2;
 	document.getElementById('operator').textContent = "/";
-
 }
 
 

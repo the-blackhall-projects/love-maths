ca66785a6c28d2c9f85d6cad77996b9ac57acd81
michael-megan <sba21652@student.cct.ie>
2023-03-22 23:35:41 +0000

    Add favicon, enter key function and clearing of answer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,15 @@ document.addEventListener("DOMContentLoaded", function() {
 				checkAnswer();
 			} else {
 				let gameType = this.getAttribute("data-type");
-
 				runGame(gameType);
-
 			}
 		})
 	}
+	document.getElementById("answer-box").addEventListener("keydown", function(event){
+		if (event.key === "Enter") {
+			checkAnswer();
+		}
+	})
 
 	runGame("addition");
 })
@@ -26,6 +29,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * been processed.
  */
 function runGame(gameType) {
+	document.getElementById("answer-box").value = "";
+	document.getElementById("answer-box").focus();
+
 	// Create two random numbers between 1 and 25
 	let num1 = Math.floor(Math.random()*25)+1;
 	let num2 = Math.floor(Math.random()*25)+1;
